a73536f09936a7f2cfc53cd401ac94796746419c
Jennifer Chan <ed18gfc@leeds.ac.uk>
2022-06-20 00:51:17 +0100

    Initial Commit

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+//Any live cell with two or three live neighbours survives.
+//Any dead cell with three live neighbours becomes a live cell.
+//All other live cells die in the next generation. Similarly, all other dead cells stay dead.
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
